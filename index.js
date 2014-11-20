ba7d385bbecfa0a78b239e34f08791a319fe3b2b
Ntw xen <myemail@xen.si>
2014-11-20 09:58:05 +0000

    debug index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express();
 //var fs = require("fs")
 index_buff = fs.readFileSync('index.html')
-index_string = index_buss.toString()
+index_string = index_buff.toString()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 

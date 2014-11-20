26d0e2457d4fddaf12d0dbba87f2c5d584fe5d70
Ntw xen <myemail@xen.si>
2014-11-20 09:53:22 +0000

    read from file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 var express = require('express')
 var app = express();
-
+//var fs = require("fs")
+index_buff = fs.readFileSync('index.html')
+index_string = index_buss.toString()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(index_string)
 })
 
 app.listen(app.get('port'), function() {

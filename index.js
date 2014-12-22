dc24ce9840936c8ab6ef1018899db9bf2a175683
Ubuntu <ubuntu@ip-172-31-11-134.ec2.internal>
2014-12-22 10:11:26 +0000

    commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var express = require('express')
 var app = express();
 var fs = require("fs")
@@ -12,5 +11,6 @@ app.get('/', function(request, response) {
 })
 
 app.listen(app.get('port'), function() {
+
   console.log("Node app is running at localhost:" + app.get('port'))
 })
